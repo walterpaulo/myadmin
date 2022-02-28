de6db5e311b2cb1219790a2739923454f5e70148
walterpaulo <walter0paulo@hotmail.com>
2022-02-28 15:18:12 -0300

    field validation

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,5 @@
 class Micropost < ApplicationRecord
   belongs_to :author, required: true
+
+  validates :name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
 end
