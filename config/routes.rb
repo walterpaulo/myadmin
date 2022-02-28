41b67c77dd19922798f38d7979d597263f1aa384
walterpaulo <walter0paulo@hotmail.com>
2022-02-28 15:49:08 -0300

    admin template

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :authors
+      resources :microposts
+
+      root to: "authors#index"
+    end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
